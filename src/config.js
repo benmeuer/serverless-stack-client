befc0f38d907fa9d3ff5e4dbc49f323686a41337
Ben Meuer <bmeuer@gmail.com>
2020-05-29 19:24:19 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "scratch-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://8nlcxlkai2.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_LCAIrWCcZ",
+    APP_CLIENT_ID: "5url6ghdsf8p9o4erfhrd8p2uk",
+    IDENTITY_POOL_ID: "us-east-2:c0bb8cde-cc47-425a-a1d1-ff64a6c37ced"
+  }
+};
